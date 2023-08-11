35753e663cac9c4f8cfd4d0a0081a55673a0cc6e
Motaz-Mukhtar <motazksa2@gmail.com>
2023-08-12 00:50:38 +0200

    Update /api/v1/auth/session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,10 +13,9 @@ class SessionExpAuth(SessionAuth):
         date to a Session ID.
     """
     def __init__(self):
-        super().__init__()
-        if os.getenv("SESSION_DURATION"):
+        try:
             self.session_duration = os.getenv("SESSION_DURATION")
-        else:
+        except Exception:
             self.session_duration = 0
 
     def create_session(self, user_id=None):
@@ -25,7 +24,7 @@ class SessionExpAuth(SessionAuth):
         """
         session_id = super().create_session(user_id)
 
-        if session_id is None:
+        if session_id is None or user_id is None:
             return None
         session_dictionary = {
                 "user_id": user_id,
